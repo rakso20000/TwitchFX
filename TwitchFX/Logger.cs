14eaeeef13b73f596c79f0b871556d0dda0996c8
rakso20000 <rakso20000@gmail.com>
2020-09-10 14:43:06 +0200

    Initialize Plugin

---
+++ TwitchFX/Logger.cs
@@ -0,0 +1,11 @@
+﻿using IPALogger = IPA.Logging.Logger;
+
+namespace TwitchFX {
+	
+	internal static class Logger {
+		
+		internal static IPALogger log { get; set; }
+		
+	}
+	
+}
---
+++ TwitchFX/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace TwitchFX {
+	
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin {
+		
+		internal static Plugin instance { get; private set; }
+		internal static string Name => "TwitchFX";
+		
+		[Init]
+		/// <summary>
+		/// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+		/// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+		/// Only use [Init] with one Constructor.
+		/// </summary>
+		public void Init(IPALogger logger, Config conf) {
+			
+			instance = this;
+			Logger.log = logger;
+			Logger.log.Debug("Logger initialized.");
+			
+			Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+			Logger.log.Debug("Config loaded");
+			
+		}
+		
+		[OnStart]
+		public void OnApplicationStart() {
+			
+			Logger.log.Debug("OnApplicationStart");
+			new GameObject("TwitchFXController").AddComponent<TwitchFXController>();
+			
+		}
+		
+		[OnExit]
+		public void OnApplicationQuit() {
+			
+			Logger.log.Debug("OnApplicationQuit");
+			
+		}
+		
+	}
+	
+}
