3c94e995676dc4748836e69107f932d4d3d6c94a
rakso20000 <rakso20000@gmail.com>
2020-09-12 22:17:12 +0200

    Add further reflection helper methods

--- TwitchFX/Helper.cs
+++ TwitchFX/Helper.cs
@@ -17,9 +17,35 @@ namespace TwitchFX {
 			
 		}
 		
+		public static T GetValue<C, T>(object obj, string name) {
+			
+			FieldInfo field = typeof(C).GetField(
+				name,
+				BindingFlags.NonPublic |
+				BindingFlags.Public |
+				BindingFlags.Instance
+			);
+			
+			return (T) field.GetValue(obj);
+			
+		}
+		
 		public static void SetValue<T>(object obj, string name, T value) {
 			
 			FieldInfo field = obj.GetType().GetField(
+				name,
+				BindingFlags.NonPublic |
+				BindingFlags.Public |
+				BindingFlags.Instance
+			);
+			
+			field.SetValue(obj, value);
+			
+		}
+		
+		public static void SetValue<C, T>(object obj, string name, T value) {
+			
+			FieldInfo field = typeof(C).GetField(
 				name,
 				BindingFlags.NonPublic |
 				BindingFlags.Public |
