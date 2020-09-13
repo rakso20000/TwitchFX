38dde69e50a06829db975677b7b5cf157665b91e
rakso20000 <rakso20000@gmail.com>
2020-09-13 16:53:54 +0200

    Remove ConfigurableColorSO
    Setting colors via ColorSOs in LightSwitchEventEffect is overriden
    by Chroma and is therefore completely replaced by using the
    LightWithIdManagerWrapper.

--- TwitchFX/LightController.cs
+++ TwitchFX/LightController.cs
@@ -12,11 +12,6 @@ namespace TwitchFX {
 		
 		private LightWithIdManagerWrapper managerWrapper;
 		
-		private ConfigurableColorSO colorLeft;
-		private ConfigurableColorSO colorRight;
-		private ConfigurableColorSO highlightcolorLeft;
-		private ConfigurableColorSO highlightcolorRight;
-		
 		private LightSwitchEventEffect[] lights;
 		
 		private void Awake() {
@@ -42,26 +37,9 @@ namespace TwitchFX {
 			LightSwitchEventEffect ligt = lights[0];
 			managerWrapper = new LightWithIdManagerWrapper(Helper.GetValue<LightWithIdManager>(ligt, "_lightManager"));
 			
-			colorRight = ScriptableObject.CreateInstance<ConfigurableColorSO>();
-			colorLeft = ScriptableObject.CreateInstance<ConfigurableColorSO>();
-			highlightcolorRight = ScriptableObject.CreateInstance<ConfigurableColorSO>();
-			highlightcolorLeft = ScriptableObject.CreateInstance<ConfigurableColorSO>();
-			
-			Color offColor = Helper.GetValue<Color>(ligt, "_offColor");
-			
-			colorRight.Init(Helper.GetValue<ColorSO>(ligt, "_lightColor0"), offColor);
-			colorLeft.Init(Helper.GetValue<ColorSO>(ligt, "_lightColor1"), offColor);
-			highlightcolorRight.Init(Helper.GetValue<ColorSO>(ligt, "_highlightColor0"), offColor);
-			highlightcolorLeft.Init(Helper.GetValue<ColorSO>(ligt, "_highlightColor1"), offColor);
-			
 			foreach(LightSwitchEventEffect light in lights) {
 				
 				Helper.SetValue<LightWithIdManagerWrapper>(light, "_lightManager", managerWrapper);
-				
-				Helper.SetValue<ColorSO>(light, "_lightColor0", colorRight);
-				Helper.SetValue<ColorSO>(light, "_lightColor1", colorLeft);
-				Helper.SetValue<ColorSO>(light, "_highlightColor0", highlightcolorRight);
-				Helper.SetValue<ColorSO>(light, "_highlightColor1", highlightcolorLeft);
 				
 			}
 			
@@ -69,55 +47,15 @@ namespace TwitchFX {
 		
 		public void SetLeftColor(Color color) {
 			
-			colorLeft.SetColor(color);
-			highlightcolorLeft.SetColor(color);
-			
 		}
 		
 		public void SetRightColor(Color color) {
-			
-			colorRight.SetColor(color);
-			highlightcolorRight.SetColor(color);
 			
 		}
 		
 		public void UpdateLights(ColorMode mode) {
 			
 			overrideLights = mode != ColorMode.Default;
-			
-			colorLeft.SetMode(mode);
-			colorRight.SetMode(mode);
-			highlightcolorLeft.SetMode(mode);
-			highlightcolorRight.SetMode(mode);
-			
-			foreach (LightSwitchEventEffect light in lights) {
-				
-				int oldEventData = Helper.GetValue<int>(light, "_prevLightSwitchBeatmapEventDataValue");
-				int newEventData;
-				
-				switch (oldEventData) {
-				case 0: //off
-					return;
-				case 1: //on
-				case 5: //on
-					newEventData = oldEventData;
-					break;
-				case 2: //flash
-				case 6: //flash
-					newEventData = oldEventData - 1;
-					break;
-				case 3: //fade
-				case 7: //fade
-				case -1: //fade
-					newEventData = 0;
-					break;
-				default:
-					return;
-				}
-				
-				light.ProcessLightSwitchEvent(newEventData, false);
-				
-			}
 			
 		}
 		
@@ -126,11 +64,6 @@ namespace TwitchFX {
 			if (disableOn != -1f && Time.time > disableOn) {
 				
 				disableOn = -1f;
-				
-				colorLeft.SetMode(ColorMode.Default);
-				colorRight.SetMode(ColorMode.Default);
-				highlightcolorLeft.SetMode(ColorMode.Default);
-				highlightcolorRight.SetMode(ColorMode.Default);
 				
 			}
 			
