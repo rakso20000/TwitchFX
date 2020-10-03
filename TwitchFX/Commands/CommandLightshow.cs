10ec244b65c94815985f8b82b83e7d1b20c761d0
rakso20000 <rakso20000@gmail.com>
2020-10-03 21:39:32 +0200

    Add command !lightshow

---
+++ TwitchFX/Commands/CommandLightshow.cs
@@ -0,0 +1,29 @@
+﻿namespace TwitchFX.Commands {
+	
+	public class CommandLightshow : Command {
+		
+		public override void Execute(string argsStr) {
+			
+			SetUsage("<name>");
+			
+			string[] args = ParseArgs(argsStr, 1);
+			string name = args[0];
+			
+			if (LightController.instance == null) {
+				
+				Plugin.chat.Send("Please use this command during a song");
+				
+				return;
+				
+			}
+			
+			bool success = LightController.instance.ShowCustomLightshow(name);
+			
+			if (!success)
+				Plugin.chat.Send("Lightshow with name " + name + " not found");
+			
+		}
+		
+	}
+	
+}
