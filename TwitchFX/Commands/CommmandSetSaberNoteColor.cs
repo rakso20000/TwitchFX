1727916d1828dedc1431e9839c20f28957c8363a
rakso20000 <rakso20000@gmail.com>
2020-10-09 16:21:31 +0200

    Add command !setsabernotecolor

---
+++ TwitchFX/Commands/CommmandSetSaberNoteColor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace TwitchFX.Commands {
+	
+	public class CommmandSetSaberNoteColor : Command {
+		
+		public override void Execute(string argsStr) {
+			
+			SetUsage("<left color> <right color> OR\n" +
+			"<left color> <right color> <duration in seconds>");
+			
+			string[] args = ParseArgs(argsStr, 2, 3);
+			
+			Color leftColor = ParseColor(args[0]);
+			Color rightColor = ParseColor(args[1]);
+			
+			float duration = TryParseFloat(args, 2);
+			
+			if (ColorController.instance == null) {
+				
+				Plugin.chat.Send("Please use this command during a song");
+				
+				return;
+				
+			}
+			
+			ColorController.instance.SetSaberColors(leftColor, rightColor);
+			ColorController.instance.SetNoteColors(leftColor, rightColor);
+			
+			if (args.Length >= 3) {
+				
+				ColorController.instance.DisableSaberColorsIn(duration);
+				ColorController.instance.DisableNoteColorsIn(duration);
+				
+			}
+			
+		}
+		
+	}
+	
+}
