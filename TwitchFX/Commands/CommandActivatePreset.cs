a550847d3b95ac1951a37068d30acdf3903f9c93
rakso20000 <rakso20000@gmail.com>
2020-10-11 18:38:37 +0200

    Add command !activatepreset

---
+++ TwitchFX/Commands/CommandActivatePreset.cs
@@ -0,0 +1,53 @@
+﻿namespace TwitchFX.Commands {
+	
+	public class CommandActivatePreset : Command {
+		
+		public override void Execute(string argsStr) {
+			
+			SetUsage("<preset> OR\n" +
+			"<preset> <duration in seconds>");
+			
+			string[] args = ParseArgs(argsStr, 1, 2);
+			string name = args[0];
+			
+			float duration = TryParseFloat(args, 1);
+			
+			if (LightController.instance == null || ColorController.instance == null) {
+				
+				Plugin.chat.Send("Please use this command during a song");
+				
+				return;
+				
+			}
+			
+			ColorPreset preset = ColorPreset.GetColorPreset(name);
+			
+			if (preset == null) {
+				
+				Plugin.chat.Send("Preset with name " + name + " not found");
+				
+				return;
+				
+			}
+			
+			LightController.instance.SetColors(preset.leftLightColor, preset.rightLightColor);
+			LightController.instance.SetColorMode(ColorMode.Custom);
+			
+			ColorController.instance.SetNoteColors(preset.leftNoteColor, preset.rightNoteColor);
+			ColorController.instance.SetSaberColors(preset.leftSaberColor, preset.rightSaberColor);
+			ColorController.instance.SetWallColor(preset.wallColor);
+			
+			if (args.Length >= 2) {
+				
+				LightController.instance.DisableIn(duration);
+				ColorController.instance.DisableNoteColorsIn(duration);
+				ColorController.instance.DisableSaberColorsIn(duration);
+				ColorController.instance.DisableWallColorIn(duration);
+				
+			}
+			
+		}
+		
+	}
+	
+}
